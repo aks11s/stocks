2681182a8d0c8b93899515fded98fa4f122f9cda
aks11s <aksis2003@mail.ru>
2023-11-09 21:25:30 +0300

    wire HomeHeaderView into HomeViewController

--- stocks/Presentation/Features/Home/HomeViewController.swift
+++ stocks/Presentation/Features/Home/HomeViewController.swift
@@ -1,9 +1,32 @@
 import UIKit
+import SnapKit
 
 final class HomeViewController: UIViewController {
+
+    // MARK: - Subviews
+
+    private lazy var headerView = HomeHeaderView()
+
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .appBackground
+        setupViews()
+        setupLayout()
+    }
+
+    // MARK: - Setup
+
+    private func setupViews() {
+        view.addSubview(headerView)
+    }
+
+    private func setupLayout() {
+        // Header extends behind the status bar and stops 51pt below the safe area top
+        headerView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.top).offset(51)
+        }
     }
 }
