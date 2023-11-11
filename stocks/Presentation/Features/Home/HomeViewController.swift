7e74c7418fd64b49b490e05000ea5a8916f42dc8
aks11s <aksis2003@mail.ru>
2023-11-11 16:07:17 +0300

    add CoinCardView with sparkline chart

--- stocks/Presentation/Features/Home/HomeViewController.swift
+++ stocks/Presentation/Features/Home/HomeViewController.swift
@@ -6,6 +6,21 @@ final class HomeViewController: UIViewController {
     // MARK: - Subviews
 
     private lazy var headerView = HomeHeaderView()
+    private lazy var quickActionsView = QuickActionsView()
+
+    // White scrollable area below the dark section
+    private lazy var scrollView: UIScrollView = {
+        let sv = UIScrollView()
+        sv.backgroundColor = .white
+        sv.showsVerticalScrollIndicator = false
+        return sv
+    }()
+
+    private lazy var contentView: UIView = {
+        let v = UIView()
+        v.backgroundColor = .white
+        return v
+    }()
 
     // MARK: - Lifecycle
 
@@ -19,14 +34,38 @@ final class HomeViewController: UIViewController {
     // MARK: - Setup
 
     private func setupViews() {
-        view.addSubview(headerView)
+        // Header and quick actions pinned directly to view — fixed, no scroll
+        [headerView, quickActionsView].forEach { view.addSubview($0) }
+
+        // Everything else scrolls
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
     }
 
     private func setupLayout() {
-        // Header extends behind the status bar and stops 51pt below the safe area top
+        // Header: top of view → extends behind status bar; height = safeArea.top + 51pt
         headerView.snp.makeConstraints { make in
             make.top.leading.trailing.equalToSuperview()
             make.bottom.equalTo(view.safeAreaLayoutGuide.snp.top).offset(51)
+        }
+
+        // Quick actions: fixed below header, 168pt tall
+        quickActionsView.snp.makeConstraints { make in
+            make.top.equalTo(headerView.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(168)
+        }
+
+        scrollView.snp.makeConstraints { make in
+            make.top.equalTo(quickActionsView.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+            // Bottom pinned to safeArea so content doesn't hide behind tab bar
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+        }
+
+        contentView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+            make.width.equalTo(scrollView)
         }
     }
 }
