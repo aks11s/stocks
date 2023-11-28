5b89682b4cc3414754311cde50570bd44263cdf2
aks11s <aksis2003@mail.ru>
2023-11-28 10:12:34 +0300

    profileStorage, profileScreen to userdefaults

---
+++ stocks/Data/Persistence/ProfileStorage.swift
@@ -0,0 +1,56 @@
+import Foundation
+
+// Single source of truth for user profile data — all fields are optional
+// because a fresh install has nothing saved yet.
+final class ProfileStorage {
+
+    static let shared = ProfileStorage()
+    private init() {}
+
+    private let defaults = UserDefaults.standard
+
+    private enum Key {
+        static let username = "profile.username"
+        static let email    = "profile.email"
+        static let phone    = "profile.phone"
+        static let password = "profile.passwordHash"
+    }
+
+    var username: String? {
+        get { defaults.string(forKey: Key.username) }
+        set { defaults.set(newValue, forKey: Key.username) }
+    }
+
+    var email: String? {
+        get { defaults.string(forKey: Key.email) }
+        set { defaults.set(newValue, forKey: Key.email) }
+    }
+
+    var phone: String? {
+        get { defaults.string(forKey: Key.phone) }
+        set { defaults.set(newValue, forKey: Key.phone) }
+    }
+
+    // Store only a simple hash — never keep plaintext passwords on device
+    var passwordHash: String? {
+        get { defaults.string(forKey: Key.password) }
+        set { defaults.set(newValue, forKey: Key.password) }
+    }
+
+    func save(username: String?, email: String?, phone: String?, password: String?) {
+        self.username = username?.isEmpty == false ? username : nil
+        self.email    = email?.isEmpty    == false ? email    : nil
+        self.phone    = phone?.isEmpty    == false ? phone    : nil
+        if let raw = password, !raw.isEmpty {
+            self.passwordHash = raw.simpleHash
+        }
+    }
+}
+
+// Lightweight deterministic hash — good enough for local storage,
+// not intended as a cryptographic solution.
+private extension String {
+    var simpleHash: String {
+        String(self.unicodeScalars.reduce(5381) { ($0 << 5) &+ $0 &+ UInt64($1.value) })
+    }
+}
